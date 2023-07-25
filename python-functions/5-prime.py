23fa02fa1b62efe559a34ccba8883835b0c5b1c2
thayour20 <suleimanridwan@gmail.com>
2023-07-25 23:20:01 +0100

    prime

---
+++ python-functions/5-prime.py
@@ -0,0 +1,12 @@
+#!/usr/bin/ env python3
+def is_prime(number):
+    prime = True
+    if number <= 0:
+        prime = False
+    for i in range (2, number):
+        if number % i == 0:
+            prime = False
+    return (prime)
+
+is_prime(5)
+print(is_prime(5))
