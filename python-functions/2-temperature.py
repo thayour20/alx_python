ae80188c9a3006cb3f7eb220f090c6adbada583f
thayour20 <suleimanridwan@gmail.com>
2023-07-25 18:21:47 +0100

    f-convert corr

--- python-functions/2-temperature.py
+++ python-functions/2-temperature.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 def convert_to_celsius(fahrenheit):
 
-    celsius = (fahrenheit - 32) * (5 /9)
+    celsius = (fahrenheit - 32) * 5 /9
     return (celsius)
+convert_to_celsius(3)
+
+print(convert_to_celsius(-459.67))
