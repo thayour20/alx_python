d11a3ba1e42c181c64d9a2bf97b5ff9b824417ea
thayour20 <suleimanridwan@gmail.com>
2023-07-25 22:44:00 +0100

    fib

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -6,7 +6,3 @@ def fibonacci_sequence(n):
         next_fibonacci = fibonacci_list[-1] + fibonacci_list[-2]
         fibonacci_list.append(next_fibonacci)
     return (fibonacci_list)
-
-
-fibonacci_sequence(5)
-print(fibonacci_sequence(5))
