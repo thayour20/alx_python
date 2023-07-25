808036067a5ed6bce6c8a44bb32f703da5e4bed9
thayour20 <suleimanridwan@gmail.com>
2023-07-25 22:41:20 +0100

    fibo corr2

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -1,17 +1,12 @@
 #!/usr/bin/env python3
 
 def fibonacci_sequence(n):
-    if n == 0:
-        return ([])
-    elif n == 1:
-        return ([0])
-    elif n >= 2:
-        fibonacci_list = [0, 1]
-        while len(fibonacci_list) < n:
-            next_fibonacci = fibonacci_list[-1] + fibonacci_list[-2]
-            fibonacci_list.append(next_fibonacci)
-        return (fibonacci_list)
+    fibonacci_list = [0, 1]
+    while len(fibonacci_list) < n:
+        next_fibonacci = fibonacci_list[-1] + fibonacci_list[-2]
+        fibonacci_list.append(next_fibonacci)
+    return (fibonacci_list)
 
 
 fibonacci_sequence(5)
-print(fibonacci_sequence(0))
+print(fibonacci_sequence(5))
