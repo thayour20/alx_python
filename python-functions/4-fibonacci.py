898046a13db34c49379ab9ec94e60355a80743d6
thayour20 <suleimanridwan@gmail.com>
2023-07-26 21:53:08 +0100

    fibonacci corr 4

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3
 
 def fibonacci_sequence(n):
-    if n == 0:
+    if n <= 0:
         return ([])
     elif n == 1:
         return ([0])
 
     fibonacci_list = [0, 1]
-    while len(fibonacci_list) < n:
+    for i in range(2, n):
         next_fibonacci = fibonacci_list[-1] + fibonacci_list[-2]
+        if next_fibonacci < 0:
+            return ([])
         fibonacci_list.append(next_fibonacci)
     return (fibonacci_list)
+fibonacci_sequence(5)
+print(fibonacci_sequence(20))
--- python-functions/5-prime.py
+++ python-functions/5-prime.py
@@ -3,10 +3,12 @@ def is_prime(number):
     prime = True
     if number <= 0:
         prime = False
-    for i in range (2, number):
-        if number % i == 0:
-            prime = False
-    return (prime)
+    elif number > 1:
+
+        for i in range (2, number):
+            if number % i == 0:
+                prime = False
+        return (prime)
 
 is_prime(5)
-print(is_prime(5))
+print(is_prime(-5))
