89728f271ec6f756b88f1f10127c9cce83e8728d
thayour20 <suleimanridwan@gmail.com>
2023-07-26 22:16:44 +0100

    f-prime corr

--- python-functions/5-prime.py
+++ python-functions/5-prime.py
@@ -8,5 +8,5 @@ def is_prime(number):
         for i in range (2, number):
             if number % i == 0:
                 prime = False
-                break
+                
     return (prime)
