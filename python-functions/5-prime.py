db866d6e7b9a90e8504293e6ee02063430c39214
thayour20 <suleimanridwan@gmail.com>
2023-07-26 22:46:36 +0100

    prime corr6

--- python-functions/5-prime.py
+++ python-functions/5-prime.py
@@ -1,10 +1,17 @@
 #!/usr/bin/ env python3
 def is_prime(number):
-    prime = True
-    if number <= 0:
-        prime = False
-    elif number > 1:
-        for i in range (2, number):
-            if number % i == 0:
-                prime = False            
-    return (prime)
+    if number <= 1:
+        return (False)
+
+    elif number == 2:
+        return (True)
+    elif number % 2 == 0:
+        return (False)
+    
+    i = 3
+    while i * i <= number:
+        if number % i == 0:
+            return (False)
+        i += 2
+
+    return (True) 
