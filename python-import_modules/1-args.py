3862375505ab3cbd057fc215393a1052d42d5ede
thayour20 <suleimanridwan@gmail.com>
2023-07-28 18:58:51 +0100

    arg1

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -2,13 +2,14 @@
 from sys import argv
 
 some_arg = len(argv) - 1
+if __name__ == "__main__":
 
-if some_arg == 0:
-    print("0 arguments.")
-elif some_arg == 1:
-    print("1 argument:")
-else
-    print("{} arguments:".format(some_arg))
+    if some_arg == 0:
+        print("0 arguments.")
+    elif some_arg == 1:
+        print("1 argument:")
+    else
+        print("{} arguments:".format(some_arg))
 
-for i, arg in enumerate(argv[1:], start= 1):
-    print("{}: {}".format(i, arg))
+    for i, arg in enumerate(argv[1:], start= 1):
+        print("{}: {}".format(i, arg))
