32d931af0cdcc0d3ee2faf1dd88d301ed6fe2371
thayour20 <suleimanridwan@gmail.com>
2023-07-28 20:46:32 +0100

    arg corr 3

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     if sum_arg == 0:
         print("0 arguments.")
     elif sum_arg == 1:
-        print("1 arguments:")
+        print("1 argument:")
     else:
         print("{}: arguments:".format(sum_arg))
 
