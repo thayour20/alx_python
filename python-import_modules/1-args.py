cd94bd1b1186b5f230ab647d6958880edc4b35f1
thayour20 <suleimanridwan@gmail.com>
2023-07-28 18:55:51 +0100

    arg

---
+++ python-import_modules/1-args.py
@@ -0,0 +1,14 @@
+#!/bin/usr/env python3
+from sys import argv
+
+some_arg = len(argv) - 1
+
+if some_arg == 0:
+    print("0 arguments.")
+elif some_arg == 1:
+    print("1 argument:")
+else
+    print("{} arguments:".format(some_arg))
+
+for i, arg in enumerate(argv[1:], start= 1):
+    print("{}: {}".format(i, arg))
