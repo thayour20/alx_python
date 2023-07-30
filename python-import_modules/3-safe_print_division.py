a1f734860e4cafb95a4ca597296c774c3f5956e9
thayour20 <suleimanridwan@gmail.com>
2023-07-30 21:33:39 +0100

    div corr

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -1,16 +1,11 @@
 #!/bin/usr/python3
 def safe_print_division(a, b):
     
-    zero = "None"
     try:
         division = a / b
     except ZeroDivisionError:
         return (None)
     finally:
-        if division == 0:
-            print("Inside result: {}".format(division))
-            return (division)
-a = 10
-b = 2
-result = safe_print_division(a, b)
-print("{:d} / {:d} = {}".format(a, b, result))
+
+        print("Inside result: {}".format(division))
+    return division
