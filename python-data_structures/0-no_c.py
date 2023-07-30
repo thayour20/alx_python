78388a37b6bb3840292f5800e277b6a2cdf79d82
thayour20 <suleimanridwan@gmail.com>
2023-07-30 22:32:31 +0100

    D struct 0

---
+++ python-data_structures/0-no_c.py
@@ -0,0 +1,9 @@
+def no_c(my_string):
+    
+    new_string = ""
+    for i in (my_string):
+        if i == 'c' or i == 'C':
+            pass
+        else:
+            new_string +=i
+    return new_string
