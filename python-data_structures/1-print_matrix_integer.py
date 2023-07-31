a9ad4cab6f74eab3502e6fa9cfe484aa75f20cee
thayour20 <suleimanridwan@gmail.com>
2023-07-31 10:25:43 +0100

    matrix corr 5

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -3,5 +3,13 @@ def print_matrix_integer(matrix=[[]]):
 
     for row in matrix:
         for element in row:
-            print("{:d}".format(element, end=" "))
+            print("{:d}".format(element), end=" ")
         print()
+    
+    
+print(print_matrix_integer(matrix=[
+    [1, 5, 7],
+    [3, 7, 8],
+    [3, 4, 1]
+]
+))
