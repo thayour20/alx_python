e2ef1c53411480b2e69b064e89cb51bdf542558b
thayour20 <suleimanridwan@gmail.com>
2023-07-31 10:33:23 +0100

    mat corr 8

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -2,8 +2,8 @@
 def print_matrix_integer(matrix=[[]]):
 
     for row in matrix:
-        for element in row:
-            if element == len(row) -1:
+        for index, element in enumerate(row):
+            if index == len(row) -1:
                 print("{:d}".format(element), end="")
             else:
                 print("{:d}".format(element), end=" ")
