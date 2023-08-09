fe85f4f2dbb94c9959a02641eed639444ec3376c
thayour20 <suleimanridwan@gmail.com>
2023-08-09 14:51:04 +0100

    best key

---
+++ python-more_data_structures/3-best_score.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python 3
+def best_score(a_dictionary):
+    if best_score is None:
+        return None
+    
+    best_key = None
+    best_value = float("-inf")
+
+    for key, value in a_dictionary.items():
+        if value > best_value:
+            best_key = key
+            best_value = value
+
+    return best_key
