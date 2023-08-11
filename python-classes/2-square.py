078f4c2637beb0b577bfceead1e92721cdbe950e
thayour20 <suleimanridwan@gmail.com>
2023-08-11 12:05:50 +0100

    task 2 corr

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,22 +1,45 @@
 #!/usr/bin python 3
-
 """
-     This class represents a square.
-"""
-class square:
+This module defines a Square class that represents a square shape.
 
-    """ function to create a private isinstance size and check for if it is a positive integer"""
-    def __init__ (self, size=0):
-        
-        if not isinstance (size, int):
-            raise TypeError ("size must be an integer")
+The Square class provides a way to create square objects with a specified size.
+It also includes a method to calculate the area of the square.
+
+Attributes:
+    None
+"""
+
+class Square:
+    """
+    This class represents a square.
+
+    Attributes:
+        __size (int): The size of the square.
+    """
+
+    def __init__(self, size):
+        """
+        Initializes a Square instance with the provided size.
+
+        Args:
+            size (int): The size of the square.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
         elif size < 0:
-            raise ValueError ("size must be >= 0")
+            raise ValueError("size must be >= 0")
         
         self.__size = size
 
-    """
-    function to find the area of the square
-    """
     def area(self):
+        """
+        Calculates the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
         return self.__size * self.__size
