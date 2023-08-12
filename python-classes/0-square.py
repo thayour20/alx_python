c177249e2ca1be5b7082253e62dfaf376b7de393
thayour20 <suleimanridwan@gmail.com>
2023-08-12 15:10:25 +0100

    test 15

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -10,10 +10,12 @@ class square:
         __size (int): The size of the square.
      
     """
-    
-    def __init__(self, size):
-        """
-        Attributes:
-        __size : is private
-        """
+
+    def __init__(self, size=0):
+        
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        
         self.__size = size
