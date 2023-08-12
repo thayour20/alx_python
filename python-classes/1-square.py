425eb9a37c948916c23cc0298da3a69573c3bab3
thayour20 <suleimanridwan@gmail.com>
2023-08-12 14:47:51 +0100

    test 0

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,14 +1,4 @@
 #!/usr/bin python 3
-"""
-This module defines a Square class that represents a square shape.
-
-The Square class provides a way to create square objects with a specified size.
-It also includes a method to calculate the area of the square.
-
-Attributes:
-    None
-"""
-
 class Square:
     """
     This class represents a square.
@@ -16,9 +6,17 @@ class Square:
     Attributes:
         __size (int): The size of the square.
     """
-
     def __init__(self, size=0):
-        
+        """
+        Initializes a Square instance with an optional size.
+
+        Args:
+            size (int, optional): The size of the square. Default is 0.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
--- python-classes/test.py
+++ python-classes/test.py
@@ -12,3 +12,33 @@ try:
     print(my_square.__size)
 except Exception as e:
     print(e)
+
+
+
+
+        """
+This module defines a Square class that represents a square shape.
+
+The Square class provides a way to create square objects with a specified size.
+It also includes a method to calculate the area of the square.
+
+Attributes:
+    None
+"""
+
+class Square:
+    """
+    This class represents a square.
+
+    Attributes:
+        __size (int): The size of the square.
+    """
+
+    def __init__(self, size=0):
+        
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        
+        self.__size = size
