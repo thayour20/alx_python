dedcdd829525b6c82c7cb18b645be16672b48e64
thayour20 <suleimanridwan@gmail.com>
2023-08-12 14:59:08 +0100

    tast 7

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,20 +1,27 @@
 #!/usr/bin python 3
+"""
+This module defines a Square class that represents a square shape.
 
+The Square class provides a way to create square objects with a specified size.
+It also includes a method to calculate the area of the square.
+
+Attributes:
+    None
 """
-     This class represents a square.
-"""
-class square:
-     
+
+class Square:
     """
-    This is for different object diclearation to represents a square.
+    This class represents a square.
 
     Attributes:
         __size (int): The size of the square.
-     
     """
-    def __init__ (self, size=0):
-        """
-        Attributes:
-        __size : is private
-        """
+
+    def __init__(self, size=0):
+        
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        
         self.__size = size
