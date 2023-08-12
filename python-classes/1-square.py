15504d417a47e592d1372e38a8d841526ead1c9b
thayour20 <suleimanridwan@gmail.com>
2023-08-12 14:37:28 +0100

    corr 1 1

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -3,10 +3,17 @@ class square:
 
     """ function to create a private isinstance size and check for if it is a positive integer"""
     def __init__ (self, size):
-        
         """
-            the if statement is to check for the instance of the size
+        Initializes a Square instance with an optional size.
+
+        Args:
+            size (int, optional): The size of the square. Default is 0.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
         """
+
         if not isinstance (size, int):
             raise TypeError ("It is not an integer")
         elif size < 0:
