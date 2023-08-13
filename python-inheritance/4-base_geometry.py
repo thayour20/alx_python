45bcfcf696a411534ffb58e34b2c99105517b9ce
thayour20 <suleimanridwan@gmail.com>
2023-08-13 23:11:27 +0100

    task 4

---
+++ python-inheritance/4-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+    an emply class of Basegeometry
+"""
+class BaseGeometry:
+    """
+        function to raise an exception statement.
+    """
+    def area(self):
+        raise Exception ("area() is not implemented")
