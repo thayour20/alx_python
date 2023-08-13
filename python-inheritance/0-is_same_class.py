6099f2d1a075fea5f75c45af8b17d5058fdbe9ec
thayour20 <suleimanridwan@gmail.com>
2023-08-13 18:08:25 +0100

    inhr cor1

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python 3
+
+"""
+    function to check for isinstance of an object in a class 
+"""
 def is_same_class(obj, a_class):
+    """
+        If/else statement to check for tne isinstance
+        Return: True if yes and False if no
+    """
     if isinstance (obj, a_class):
         return True
     else:
