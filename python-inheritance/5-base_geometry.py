bed71ed1564926be3cac86c2ee8d26dc99f16e86
thayour20 <suleimanridwan@gmail.com>
2023-08-16 23:15:35 +0100

    gg

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -29,6 +29,6 @@ class BaseGeometry(metaclass=BaseMeta):
         name =''
 
         if not isinstance (value, int):
-            raise TypeError ("<name> must be an integer")
+            raise TypeError ("{} must be an integer".format(name))
         elif value <= 0:
-            raise ValueError ("<name> must be greater than 0")
+            raise ValueError ("{} must be greater than 0".format(name))
