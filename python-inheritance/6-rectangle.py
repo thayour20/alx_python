5610b577e9beefa0b4ca29f89fb514bbeeede595
thayour20 <suleimanridwan@gmail.com>
2023-08-17 22:17:40 +0100

    reactangle4

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -1,47 +1,13 @@
-#!/usr/bin/python3
+#!/usr/bin/python3   
 """
-    an emply class of Basegeometry
+     rectangle declearation function
 """
-   
-class BaseMeta(type):
+BaseGeometry = __import__ ('5-base_geometry.py').BaseGeometry
+class Rectangle(BaseGeometry):
     """
-        empty class with a pass statement.
-    """  
-    def __dir__(cls) -> None:
-        original_dir = super().__dir__()
-        return [item for item in original_dir if item != '__init_subclass__' ]
-        
-class BaseGeometry(metaclass=BaseMeta):
+        initialization
     """
-        class with a cls and area functiions.
-    """
-    def __dir__(cls) -> None:
-        original_dir = super().__dir__()
-        return [item for item in original_dir if item != '__init_subclass__' ]
-    def __init__ (self):
-        pass
-
-    def area(self):
-        """
-        rectangle declearation function
-        """
-        raise Exception ("area() is not implemented")
     
-    
-    def integer_validator(self, name = str, value = int):
-        """
-        rectangle declearation function
-        """
-
-        if not isinstance (value, int):
-            raise TypeError ("{} must be an integer".format(name))
-        elif value <= 0:
-            raise ValueError ("{} must be greater than 0".format(name))
-        
-    """
-        rectangle declearation function
-    """
-
     def __init__(self, width, height):
         """
         integer validation and private class declearation
