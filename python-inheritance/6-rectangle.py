436d97110cc1748350d717143f26eaf913adc7bf
thayour20 <suleimanridwan@gmail.com>
2023-08-17 22:42:15 +0100

    rec

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -2,7 +2,7 @@
 """
      rectangle declearation function
 """
-BaseGeometry = __import__ ('5-base_geometry.py').BaseGeometry
+#BaseGeometry = __import__ ('5-base_geometry.py').BaseGeometry
 """
      rectangle declearation function
 """
@@ -16,9 +16,7 @@ class Rectangle(BaseGeometry):
         integer validation and private class declearation
         """
 
-        if not isinstance (width, int):
-            raise TypeError ("width must be an integer")
-        elif not isinstance (height, int):
-            raise TypeError ("height must be an integer")
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
         self.__width = width
         self.__height = height
