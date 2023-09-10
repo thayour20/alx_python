a52912a910ae834e2fbb8cbb93a5386d40790c2f
thayour20 <suleimanridwan@gmail.com>
2023-09-10 18:16:10 +0100

    wws

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-from base import Base
+
+from models.base import Base
+
+"""Rectangle class, inherits from Base."""
+
 
 class Rectangle(Base):
     """Rectangle class, inherits from Base."""
