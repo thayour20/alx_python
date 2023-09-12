347af257c5c7cc672cc2f4eca0eb278765238334
thayour20 <suleimanridwan@gmail.com>
2023-09-12 23:05:04 +0100

    sq3

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -30,7 +30,7 @@ class Square (Rectangle):
             str function
         """
 
-        return "[Square] ({}) {}/{}".format(self.id, self.x, self.y, self.width)
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
     
     @property
     def size(self):
