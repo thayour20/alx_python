5bceccea1ada58140ff473fafd456be2f165f5f8
thayour20 <suleimanridwan@gmail.com>
2023-09-30 16:08:25 +0100

    ww

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -5,6 +5,12 @@ import requests
 url = 'https://alu-intranet.hbtn.io/status'
 response = requests.get(url)
 
+
+"""
 print("Body response:")
 print("     -type:", type(response.text))
 print("     -content:", response.text)
+"""
+print("Body response:")
+print("    - type:", type(response.text))
+print("    - content:", response.text)
