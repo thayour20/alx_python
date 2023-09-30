a168cbee60b0ddf6be58dbcd2f10cdfee28f654c
thayour20 <suleimanridwan@gmail.com>
2023-09-30 16:15:36 +0100

    qss

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -5,12 +5,6 @@ import requests
 url = 'https://alu-intranet.hbtn.io/status'
 response = requests.get(url)
 
-
-"""
-print("Body response:")
-print("     -type:", type(response.text))
-print("     -content:", response.text)
-"""
 print("Body response:")
 print("\t- type:", type(response.text))
 print("\t- content:", response.text)
