c124a27eb9106e215ad60b5132cdcecb9dcd9ccd
thayour20 <suleimanridwan@gmail.com>
2023-09-30 15:46:35 +0100

    sds

---
+++ python-network_1/0-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+import requests
+
+url = 'https://alu-intranet.hbtn.io/status'
+response = requests.get(url)
+
+print("Body response:")
+print("     -type", type(response.text))
+print("     -content:", response.text)
