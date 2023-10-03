b1c94dbfd4b164de0f2db76bbde830d6213d9095
thayour20 <suleimanridwan@gmail.com>
2023-10-03 21:23:17 +0100

    error

---
+++ python-network_1/4-error_code.py
@@ -0,0 +1,17 @@
+#!/bin/usr/python
+
+import requests
+import sys
+
+if len(sys.argv) != 2:
+    print("Usage: ./4-error_code.py <url>")
+    sys.exit(1)
+
+url = sys.argv[1]
+
+response = requests.get(url)
+
+print(response.text)
+
+if response.status_code <= 400:
+    print("Error code:", response.status_code)
