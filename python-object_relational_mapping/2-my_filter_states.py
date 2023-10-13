71ab4287744a5b3598d3a5df31fbda8219ba554a
thayour20 <suleimanridwan@gmail.com>
2023-10-13 15:35:07 +0100

    gii

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
 
     cursor = db.cursor()
 
-    cursor.execute("SELECT * FROM states WHERE NAME LIKE {:s} ORDER BY id ASC ".format (sys.argv[4]))
+    cursor.execute("SELECT * FROM states WHERE NAME LIKE '{:s}' ORDER BY id ASC ".format (sys.argv[4]))
 
     rows = cursor.fetchall()
 
