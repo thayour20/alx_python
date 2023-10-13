ffed3e6d7883a2775a70ddbf46acb3cf639b3dff
thayour20 <suleimanridwan@gmail.com>
2023-10-13 18:20:02 +0100

    excellent

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -16,7 +16,9 @@ if __name__ == "__main__":
     )
 
     cursor = db.cursor()
-    cursor.execute ("SELECT cities.id, cities.name, states.name FROM cities \ JOIN states ON cities.state_id = states.id  ORDER BY cities.id ASC")
+    
+    cursor.execute ("SELECT cities.id, cities.name, states.name FROM cities \
+                    JOIN states ON cities.state_id = states.id  ORDER BY cities.id ASC")
 
     results = cursor.fetchall()
 
